d3346e44f8e60a37ecabb986958d086c552fda90
root <root@7e6671743fe6>
2020-07-15 21:57:20 +0000

    Different methods

--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -1,4 +1,6 @@
 package _03_method_writing._2_happy_pet;
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class HappyPet {
@@ -12,6 +14,8 @@ String pet = JOptionPane.showInputDialog("What kind of animal would you like?");
 String name = JOptionPane.showInputDialog("What's your pet's name?");	
 
 // 7. REPEAT steps 3 - 6 enough times to make your pet happy!
+		 while (HappyPet.happinessLevel < 20) {
+			
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
@@ -20,55 +24,71 @@ String name = JOptionPane.showInputDialog("What's your pet's name?");
 					new String[] { "Play with your pet", "Clean up your pet's poo", "Go outside and explore with your pet" }, null);
 
 			// 5. Use user input to call the appropriate method created in step 4.
-
-			// 6. If you determine the happiness level is large enough, tell the
-			//    user that he loves his pet and use break; to exit for loop.
-
+if (task == 0) {
+	Response1(pet, name);
+}
+else if (task == 1) {
+	Response2(pet, name);
+}
+else  {
+	Response3(pet, name);
+}
+		 
+// 6. If you determine the happiness level is large enough, tell the
+		 			//    user that he loves his pet and use break; to exit for loop.
+if (HappyPet.happinessLevel > 20) {
+	JOptionPane.showMessageDialog(null, "You love your pet!");
+}
+		 }
 	}
-
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
-     static void Response1a(String pet, String name) {
+     static void Response1 (String pet, String name) {
+	int ran = new Random().nextInt(3);
+    	 if (ran == 0) {
 	JOptionPane.showMessageDialog(null, "Your " + pet + ", " + name + ", is happy and energized. Their happiness level has increased.");
 happinessLevel +=1;
-     }
-     static void Response1b(String pet, String name) {
+    	 }
+    	 if (ran == 1) {
     	 JOptionPane.showMessageDialog(null, "Your " + pet + ", " + name + ", is frustrated. You put barely any effort to play with them. Their happiness level has decreased.");	
      happinessLevel -=3;
-     }
-     static void Response1c(String pet, String name) {
+    	 }
+    if (ran == 2) {
     	 JOptionPane.showMessageDialog(null, "Your " + pet + ", " + name + ", really enjoyed playing with you, and was grateful that you played with them for a long time.Their happiness level has increased.");
 	happinessLevel +=2;
-     }
-     static void Response2a(String pet, String name) {
-    	 JOptionPane.showMessageDialog(null, "Your " + pet + ", " + name + ", is happy that all their poo is all cleaned up and that it doesn't smell as bad. Their happiness level has increased.");
+    }
+    }
+     static void Response2 (String pet, String name) {
+    	 int ran = new Random().nextInt(3);
+    	 if (ran == 0) {
+    	JOptionPane.showMessageDialog(null, "Your " + pet + ", " + name + ", is happy that all their poo is all cleaned up and that it doesn't smell as bad. Their happiness level has increased.");
 happinessLevel +=3;
      } 
-     static void Response2b(String pet, String name) {
+    if (ran == 1) {
     	 JOptionPane.showMessageDialog(null, "Your " + pet + ", " + name + ", doesn't like that you didn't clean the poo very well and that there is some left. Their happiness level has decreased.");
 happinessLevel --;
   }
-     static void Response2c(String pet, String name) {
+    if (ran == 2) {
     	 JOptionPane.showMessageDialog(null, "Your " + pet + ", " + name + ", isn't happy that it took you 3 days to clean their poo. Their happiness level has decreased.");
 	happinessLevel -=2;
 }
-  static void Response3a(String pet, String name) {
+}
+  static void Response3(String pet, String name) {
+	  int ran = new Random().nextInt(3);
+	  if (ran == 0) {
 	  JOptionPane.showMessageDialog(null, "Your " + pet + ", " + name + ", is very happy because you found a really nice clearing where you and them had a splendid time!!! Their happiness level has increased.");
 happinessLevel +=4;
 } 
-static void Response3b(String pet, String name) {
+	  if (ran == 1) {
 	JOptionPane.showMessageDialog(null, "Your " + pet + ", " + name + ", is upset because you instantly managed to hurt yourself and had a scratch on your foot so you went back home with them. They also didn't like seeing you upset about getting the scratch. Their happiness level has decreased.");
 happinessLevel -=3;
 } 
-static void Response3c(String pet, String name) {
+	  if (ran == 2) {
 	JOptionPane.showMessageDialog(null, "Your " + pet + ", " + name + ", enjoyed themselves but you explored for a short amount of time. Their happiness level is the same.");
 
 }
-
-
-
-
+}
 }
      
      
